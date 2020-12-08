c3f7ef1183053c00c812705ab665f03dc44c85d5
Junior <jose@vert.se>
2020-12-08 19:30:53 -0300

    Configurações iniciais do projeto

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+  preset: "@vue/cli-plugin-unit-jest",
+  testMatch: [
+    "<rootDir>/src/**/*.(spec|test).(ts|js)",
+    "<rootDir>/**/**/*.(spec|test).(ts|js)",
+  ],
 };
